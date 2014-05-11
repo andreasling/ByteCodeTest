e8be7ba79e24295b3d5a98172772921a63098489
Andreas Ling <andreas.ling@gmail.com>
2014-05-11 19:30:16 +0200

    added compiler

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ var rl = readline.createInterface({
   output: process.stdout
 });
 
-var interpret = function interpret(instructions, stack, callback) {
+var instructionSet = {
+  Add: 0x00,
+  Read: 0x01,
+  Write: 0x02,
+  Subtract: 0x03,
+  Literal: 0x04
+};
 
-  var instructionSet = {
-    Add: 0x00,
-    Read: 0x01,
-    Write: 0x02,
-    Subtract: 0x03,
-    Literal: 0x04
-  };
+var interpret = function interpret(instructions, stack, callback) {
 
   function async(arg, callback) {
     setTimeout(callback(arg), 0);
@@ -40,7 +40,7 @@ var interpret = function interpret(instructions, stack, callback) {
 
         case instructionSet.Read:
           rl.question("input: ", function(input) {
-            stack.push(parseInt(input));
+            stack.push(parseInt(input, 10));
             series(++ip);
           });
           break;
@@ -73,8 +73,51 @@ var interpret = function interpret(instructions, stack, callback) {
 
 };
 
+var compile = function compile(string) {
+
+  var tokens = string.split(/\s+/);
+  var instructions = [];
+
+  tokens.forEach(function(token) {
+
+    switch (token) {
+
+      case "+":
+        instructions.push(instructionSet.Add);
+        break;
+
+      case "-":
+        instructions.push(instructionSet.Subtract);
+        break;
+
+      case "?":
+        instructions.push(instructionSet.Read);
+        break;
+
+      case "=":
+        instructions.push(instructionSet.Write);
+        break;
+
+      default:
+        var value = parseInt(token, 10);
+        if (!isNaN(value)) {
+          instructions.push(instructionSet.Literal);
+          instructions.push(value);
+        } else {
+          throw new Error("Invalid token: " + token);
+        }
+    }
+
+  });
+
+  return instructions;
+
+};
+
+var code = "1 2 + ? - =";
+
 var stack = [];
-var instructions = [0x04, 0x01, 0x04, 0x02, 0x00, 0x02];
+var instructions = compile(code);
 
 console.log("instructions:", instructions);
 console.log("stack:", stack);
